b9c5f26400bd2d05a6f78e9f016715b26d5ae374
Falon Darville <falondarville@gmail.com>
2018-05-05 19:46:01 -0700

    login form

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import { Form, Jumbotron, Grid, Row, Col, Image, Button, FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
 import './login.css'; 
 
 export default class Login extends Component {
-	render() {
-		return (
-			<Grid>
-				<p>The login form goes here.</p>
-			</Grid>
-			)
-	}
+
+  render() {
+    return (
+    	<Form horizontal>
+  			<FormGroup controlId="formHorizontalEmail">
+    			<Col componentClass={ControlLabel} sm={4}>
+					Email
+    		</Col>
+    		<Col sm={5}>
+      				<FormControl type="email" placeholder="Email" />
+    			</Col>
+  			</FormGroup>
+
+  			<FormGroup controlId="formHorizontalPassword">
+    			<Col componentClass={ControlLabel} sm={4}>
+      			Password
+    		</Col>
+    		<Col sm={5}>
+      		<FormControl type="password" placeholder="Password" />
+    		</Col>
+  			</FormGroup>
+
+  			<FormGroup>
+   				 <Col smOffset={4} sm={10}>
+      				<Button bsStyle='primary' type="submit">Sign in</Button>
+    			</Col>
+  			</FormGroup>
+		</Form>
+    );
+  }
 }
