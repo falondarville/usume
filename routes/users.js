a0e613863470c9c04b203d7197797f74be321cc5
Falon Darville <falondarville@gmail.com>
2018-05-07 23:19:58 -0700

    clean up mysql code:

--- routes/users.js
+++ routes/users.js
@@ -22,48 +22,20 @@ router.post('/users', function(request, response){
 	// var passingData = [email, first, last, skills];
 	// request.query
 
-	// function addUser(email, first, last, skills){
+	function addUser(email, first, last, skills){
 
-	// 	var queryString= "INSERT INTO users(email, first, last, skills) VALUES(email, first, last, skills)";
+		var queryString = "INSERT INTO users(email, first, last, skills) VALUES(?, ?, ?, ?)";
 
-	// 	connectionManager.getConnection()
-	// 		.then(function(connection){
-	// 			var query = connectionManager.prepareQuery(queryString);
-	// 			console.log("Query to execute: " + query);
-	// 			connection.query(query, function(err, result){
-	// 			console.log("Reading from inside connection query function");
-	// 			console.log("Success");
-	// 		})
-	// })
+		connection.query(queryString, [email, first, last, skills], function(err, result) {
+			response.json(result);
+		} )
+
+	}
 
 	// data obtained from React registration form is printing to console
 	console.log("Reading from post function")
-	console.log(email, first, last, skills);
+	addUser(email, first, last, skills);
 })
-
-	// response.send({
-	// 	email: email,
-	// 	// password: password, 
-	// 	first: first, 
-	// 	last: last, 
-	// 	skills: skills});
-
-	// push to mySQL
-	// var queryString = "INSERT INTO users(email, first, last, skills) VALUES(?, ?, ?, ?)";
-	// connection.query(queryString, [email, first, last, skills], function(error, result) {
-	// 	if (error) throw error;
-	// 		connection.query('SELECT * FROM users', function(err, results) {
- //        		if (err) throw err
- //        			console.log(results[0].email)
- //        			console.log(results[0].first)
- //        			console.log(results[0].last)
- //        			console.log(results[0].skills)
-	// })
-// })
-
-// app.get('/users', (req, res) => {
-//   res.send({ express: 'Hello From Express' });
-// });
 
 module.exports = router;
 
