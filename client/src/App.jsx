0511a42f2013374b889ab85bc53f79db1c092260
Falon Darville <falondarville@gmail.com>
2018-05-09 13:39:05 -0700

    add catch-all

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/home';
 import Register from './components/register';
 import Login from './components/login';
 import Navbar from './components/CustomNavbar';
 
 class App extends Component {
-  // state = {
-  //   response: ''
-  // };
-
-  // componentDidMount(){
-  //   this.CallApi()
-  //   .then(res => this.setState({ response: res.express }))
-  //   .catch(err => console.log(err));
-  // }
-
-  // callApi = async() => {
-  //   const response = await fetch('/users');
-  //   const body = await response.json();
-
-  //   if (response.status !== 200) throw Error(body.message);
-
-  //   return body;
-  // }
 
   render() {
     return (
@@ -32,9 +14,12 @@ class App extends Component {
     <Router>
       <div>
         <Navbar />
-        <Route exact path='/' component={Home} />
-        <Route path='/register' component={Register} />
-        <Route path='/login' component={Login} />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/register' component={Register} />
+          <Route path='/login' component={Login} />
+          <Route path='*' component={Home} />
+        </Switch>
       </div>
     </Router>
 
