6ace7b30423baf812095aa7e7000583c7fed962b
Falon Darville <falondarville@gmail.com>
2018-05-12 14:05:35 -0700

    conditional rendering for logged in

--- client/src/components/loggedin.jsx
+++ client/src/components/loggedin.jsx
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import axios from 'axios';
 import { Form, Jumbotron, Grid, Row, Col, Image, Button, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 import './loggedin.css'; 
+
 
 // GET from UserData table and display once the user has logged in
 export default class LoggedIn extends Component {
 
 	constructor(){
-	super();
-	this.state = {
-		user: {
-			firstName: '',
-			lastName: '',
-			email: '',
-			skills: ''
-		}
+		super();
+		this.state = {
+			user: {
+				firstName: false,
+				lastName: '',
+				email: '',
+				skills: ''
+			},
+			redirectToLogin: false
 		};
 
 		var self = this;
@@ -31,16 +33,27 @@ export default class LoggedIn extends Component {
 					email: userData.email
 				}
 			})
+
 		}).catch(function(error){
 			// redirect to login page
+			self.setState({ redirectToLogin: true });
 		})
 	}
 
-render() {
-    return (
-    	<div><h2>The resume of {this.state.user.firstName} {this.state.user.lastName} </h2>
-    	<p>Skills: {this.state.user.skills} </p>
-    	</div>
-    	);
+	render() {
+
+		const { redirectToLogin } = this.state
+
+		if(redirectToLogin) {
+			return <Redirect to={{ pathname: '/login'}} />;
+		} else if(this.state.user.firstName === false) {
+			return <div></div>
+		} else {
+			return (<div>
+				<h2>The resume of {this.state.user.firstName} {this.state.user.lastName} </h2>
+					<p>Skills: {this.state.user.skills} </p>
+				</div>);
+		}
+
 	}
 }
