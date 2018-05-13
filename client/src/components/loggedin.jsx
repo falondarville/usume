e63e858bfb871e64063be22db3e6476afcff103f
Falon Darville <falondarville@gmail.com>
2018-05-12 22:23:04 -0700

    prelim styling for resume page

--- client/src/components/loggedin.jsx
+++ client/src/components/loggedin.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Redirect, Link } from 'react-router-dom';
 import axios from 'axios';
-import { div } from 'react-bootstrap';
+import { div, Col, Well } from 'react-bootstrap';
 import './loggedin.css'; 
 
 
@@ -68,20 +68,40 @@ export default class LoggedIn extends Component {
 		} else {
 			return (<div className="container">
 				<Link className="pull-right" to="/logout">Log out</Link>
-				<h1 className="text-center resume-header">{this.state.user.firstName} {this.state.user.lastName} </h1>
+				<h1 className="text-center resume-header"><b>{this.state.user.firstName} {this.state.user.lastName} </b></h1>
 					<hr />
-					<p className="text-center">{this.state.user.email} </p>
+					<h4 className="text-center email-styling">{this.state.user.email} </h4>
 
 					<div className="container body-container">
-					<p><b>Skills: </b> {this.state.user.skills} </p>
-					<p><b>Requested Title: </b> {this.state.user.title}</p>
-					<p>{this.state.user.environment}</p>
-					<p>{this.state.user.relationships}</p>
-					<p>{this.state.user.priorities}</p>
-					<p>{this.state.user.personality}</p>
-					<p>{this.state.user.workGoals}</p>
-					<p>{this.state.user.lifeGoals}</p>
-					<p>{this.state.user.accomodations}</p>
+					<Col sm={6} >
+						<Well className="well-styling">
+						<h2 className="text-center overview">Overview</h2>
+						<p><b>Requested Title: </b> {this.state.user.title}</p>
+						<p><b>Skills: </b> {this.state.user.skills} </p>
+						</Well>
+					</Col>
+					<Col sm={6}>
+						<Well className="well-styling">
+						<h2 className="text-center goals">Work and Life Goals</h2>
+						<p><b>Work Goals: </b>{this.state.user.workGoals}</p>
+						<p><b>Life Goals: </b>{this.state.user.lifeGoals}</p>
+						</Well>
+					</Col>
+					<Col sm={6}>
+						<Well className="well-styling">
+						<h2 className="text-center workplace">Workplace Desires</h2>
+						<p><b>Ideal work environment: </b>{this.state.user.environment}</p>
+						<p><b>Ideal work relationships: </b>{this.state.user.relationships}</p>
+						<p><b>Ideal work accomodations: </b>{this.state.user.accomodations}</p>
+						</Well>
+					</Col>
+					<Col sm={6}>
+						<Well className="well-styling">
+						<h2 className="text-center personality">Personality and Priorities</h2>
+						<p><b>Priorities: </b>{this.state.user.priorities}</p>
+						<p><b>Personality: </b>{this.state.user.personality}</p>
+						</Well>
+					</Col>
 					</div>
 				</div>);
 		}
