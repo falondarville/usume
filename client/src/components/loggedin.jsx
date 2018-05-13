32df70c9db2b28d7e04e858096a198c73e732f9b
Falon Darville <falondarville@gmail.com>
2018-05-13 15:15:01 -0700

    add icons to resume page

--- client/src/components/loggedin.jsx
+++ client/src/components/loggedin.jsx
@@ -75,29 +75,29 @@ export default class LoggedIn extends Component {
 					<div className="container body-container">
 					<Col sm={6} >
 						<Well className="well-styling">
-						<h2 className="text-center overview">Overview</h2>
+						<h2 className="text-center overview"><span className="icon-star well-icon"></span>Overview</h2>
 						<p><b>Requested Title: </b> {this.state.user.title}</p>
 						<p><b>Skills: </b> {this.state.user.skills} </p>
 						</Well>
 					</Col>
 					<Col sm={6}>
 						<Well className="well-styling">
-						<h2 className="text-center goals">Work and Life Goals</h2>
+						<h2 className="text-center goals"><span className="icon-globe well-icon"></span>Work and Life Goals</h2>
 						<p><b>Work Goals: </b>{this.state.user.workGoals}</p>
 						<p><b>Life Goals: </b>{this.state.user.lifeGoals}</p>
 						</Well>
 					</Col>
 					<Col sm={6}>
 						<Well className="well-styling">
-						<h2 className="text-center workplace">Workplace Desires</h2>
-						<p><b>Ideal work environment: </b>{this.state.user.environment}</p>
+						<h2 className="text-center workplace"><span className="icon-thumbs-up well-icon"></span>Workplace Desires</h2>
+						<p><b>Ideal work environment:</b>{this.state.user.environment}</p>
 						<p><b>Ideal work relationships: </b>{this.state.user.relationships}</p>
 						<p><b>Ideal work accomodations: </b>{this.state.user.accomodations}</p>
 						</Well>
 					</Col>
 					<Col sm={6}>
 						<Well className="well-styling">
-						<h2 className="text-center personality">Personality and Priorities</h2>
+						<h2 className="text-center personality"><span className="icon-pie-chart well-icon"></span>Personality and Priorities</h2>
 						<p><b>Priorities: </b>{this.state.user.priorities}</p>
 						<p><b>Personality: </b>{this.state.user.personality}</p>
 						</Well>
